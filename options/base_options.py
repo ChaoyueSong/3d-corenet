b4ec6579e62fc8975453360c79cd5acf584de674
ChaoyueSong <56154447+ChaoyueSong@users.noreply.github.com>
2021-12-24 14:38:02 +0800

    Add files via upload

---
+++ options/base_options.py
@@ -0,0 +1,134 @@
+import sys
+import argparse
+import os
+from util import util
+import torch
+import models
+import pickle
+
+
+class BaseOptions():
+    def __init__(self):
+        self.initialized = False
+
+    def initialize(self, parser):
+        # experiment specifics
+        parser.add_argument('--gpu_ids', type=str, default='0,1,2,3', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+        parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
+        parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
+        parser.add_argument('--batchSize', type=int, default=8, help='input batch size')
+        parser.add_argument('--model', type=str, default='ver2ver', help='which model to use')
+
+        # for setting inputs
+        parser.add_argument('--dataroot', type=str, default='./npt-data')
+        parser.add_argument('--dataset_mode', type=str, default='human')
+        parser.add_argument('--nThreads', default=16, type=int, help='# threads for loading data')
+        parser.add_argument('--max_dataset_size', type=int, default=sys.maxsize, help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
+        parser.add_argument('--load_from_opt_file', action='store_true', help='load the options from checkpoints and use that as default')
+
+        # for generator
+        parser.add_argument('--netG', type=str, default='elain', help='selects model to use for netG')
+        parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in first conv layer')
+        parser.add_argument('--init_type', type=str, default='xavier', help='network initialization [normal|xavier|kaiming|orthogonal]')
+        parser.add_argument('--init_variance', type=float, default=0.02, help='variance of the initialization distribution')
+
+        self.initialized = True
+        return parser
+
+    def gather_options(self):
+        # initialize parser with basic options
+        if not self.initialized:
+            parser = argparse.ArgumentParser(
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+            parser = self.initialize(parser)
+
+        # get the basic options
+        opt, unknown = parser.parse_known_args()
+
+        # modify model-related parser options
+        model_name = opt.model
+        model_option_setter = models.get_option_setter(model_name)
+        parser = model_option_setter(parser, self.isTrain)
+
+        opt, unknown = parser.parse_known_args()
+
+        # if there is opt_file, load it.
+        # The previous default options will be overwritten
+        if opt.load_from_opt_file:
+            parser = self.update_options_from_file(parser, opt)
+
+        opt = parser.parse_args()
+        self.parser = parser
+        return opt
+
+    def print_options(self, opt):
+        message = ''
+        message += '----------------- Options ---------------\n'
+        for k, v in sorted(vars(opt).items()):
+            comment = ''
+            default = self.parser.get_default(k)
+            if v != default:
+                comment = '\t[default: %s]' % str(default)
+            message += '{:>25}: {:<30}{}\n'.format(str(k), str(v), comment)
+        message += '----------------- End -------------------'
+        print(message)
+
+    def option_file_path(self, opt, makedir=False):
+        expr_dir = opt.checkpoints_dir
+        if makedir:
+            util.mkdirs(expr_dir)
+            util.mkdirs(os.path.join(expr_dir, opt.dataset_mode))
+        file_name = os.path.join(expr_dir, opt.dataset_mode, 'opt')
+        return file_name
+
+    def save_options(self, opt):
+        file_name = self.option_file_path(opt, makedir=True)
+        with open(file_name + '.txt', 'wt') as opt_file:
+            for k, v in sorted(vars(opt).items()):
+                comment = ''
+                default = self.parser.get_default(k)
+                if v != default:
+                    comment = '\t[default: %s]' % str(default)
+                opt_file.write('{:>25}: {:<30}{}\n'.format(str(k), str(v), comment))
+
+        with open(file_name + '.pkl', 'wb') as opt_file:
+            pickle.dump(opt, opt_file)
+
+    def update_options_from_file(self, parser, opt):
+        new_opt = self.load_options(opt)
+        for k, v in sorted(vars(opt).items()):
+            if hasattr(new_opt, k) and v != getattr(new_opt, k):
+                new_val = getattr(new_opt, k)
+                parser.set_defaults(**{k: new_val})
+        return parser
+
+    def load_options(self, opt):
+        file_name = self.option_file_path(opt, makedir=False)
+        new_opt = pickle.load(open(file_name + '.pkl', 'rb'))
+        return new_opt
+
+    def parse(self, save=False):
+
+        opt = self.gather_options()  #gather options from base, train, dataset, model
+        opt.isTrain = self.isTrain   # train or test
+
+        self.print_options(opt)
+        if opt.isTrain:
+            self.save_options(opt)
+
+        # set gpu ids
+        str_ids = opt.gpu_ids.split(',')
+        opt.gpu_ids = []
+        for str_id in str_ids:
+            id = int(str_id)
+            if id >= 0:
+                opt.gpu_ids.append(id)
+        if len(opt.gpu_ids) > 0:
+            torch.cuda.set_device(opt.gpu_ids[0])
+
+        assert len(opt.gpu_ids) == 0 or opt.batchSize % len(opt.gpu_ids) == 0, \
+            "Batch size %d is wrong. It must be a multiple of # GPUs %d." \
+            % (opt.batchSize, len(opt.gpu_ids))
+
+        self.opt = opt
+        return self.opt
